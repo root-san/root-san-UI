cd11fb0cf81e3be1c490b85fc3fb819d45cf023b
toshi-pono <66683209+toshi-pono@users.noreply.github.com>
2022-12-14 18:24:27 +0900

    🐛 next-pwaの回避？

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
-const withPWA = require("next-pwa")({
+
+// next-pwa not working with appdir (next 13) and next-pwa 5.6.0
+// see here: https://github.com/shadowwalker/next-pwa/issues/424
+// const withPWA = require("next-pwa");
+
+// https://github.com/shadowwalker/next-pwa/pull/427で治らないかな:eyes:
+// 一生マージされなさそうなのでとりあえず↓を使う
+// とりあえずよくわからないのを使う><
+const withPWA = require("@ducanh2912/next-pwa").default({
   dest: "public",
+  disable: process.env.NODE_ENV === "development",
 });
 
 module.exports = withPWA({
