8f2402fbfe6462f9de0df5352a9b1d0c11d25888
toshi-pono <66683209+toshi-pono@users.noreply.github.com>
2022-12-14 21:28:38 +0900

    ⚡ add mock（動いてなかったらごめん）

--- libs/apis/index.ts
+++ libs/apis/index.ts
@@ -1,7 +1,12 @@
 import { DefaultApi, Configuration } from './generated'
 
+const basePath =
+  process.env.NODE_ENV === 'production'
+    ? 'http://localhost:8080'
+    : 'http://localhost:4010'
+
 const conf = new Configuration({
-  basePath: 'http://localhost:8080',
+  basePath,
 })
 const apis = new DefaultApi(conf)
 
