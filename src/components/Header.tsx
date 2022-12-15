21ffdd9a8a20d5e0caf6b214217c0ae1476bf423
toshi/pono000 <66683209+toshi-pono@users.noreply.github.com>
2022-12-15 11:20:46 +0900

    🌱 適当にreactに置換

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,4 @@
-import { Noto_Sans_JP } from '@next/font/google'
-
-const NotoSansJP_Bold = Noto_Sans_JP({
-  weight: '700',
-  subsets: ['japanese'],
-})
+import React from 'react'
 
 interface Props {
   title: string
@@ -15,7 +10,7 @@ const Header = ({ title, left, right }: Props) => {
   return (
     <header className="grid h-12 w-full grid-cols-[32px_1fr_32px] items-center justify-items-center py-2 px-6">
       <div>{left}</div>
-      <div className={NotoSansJP_Bold.className + ' text-lg'}>{title}</div>
+      <div className="text-lg">{title}</div>
       <div>{right}</div>
     </header>
   )
