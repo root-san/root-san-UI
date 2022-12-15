cd9f99024701abc5822c85080618275f08a77e2c
toshi/pono000 <66683209+toshi-pono@users.noreply.github.com>
2022-12-15 11:45:34 +0900

    🆙 tailwind周りのセットアップ

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import path from 'path'
+
+const srcPath = path.resolve(__dirname, 'src').replace(/\\/g, '/')
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '/@': srcPath,
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "${srcPath}/styles/common.scss";`,
+      },
+    },
+  },
 })
