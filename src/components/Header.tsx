900fa213b60363e382b22dc6c990deae20c61f6a
toshi-pono <66683209+toshi-pono@users.noreply.github.com>
2022-12-16 03:42:20 +0900

    🎨  font-boldに

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,7 +10,7 @@ const Header = ({ title, left, right }: Props) => {
   return (
     <header className="grid h-12 w-full grid-cols-[32px_1fr_32px] items-center justify-items-center py-2 px-6">
       <div>{left}</div>
-      <div className="text-lg">{title}</div>
+      <div className="text-lg font-bold">{title}</div>
       <div>{right}</div>
     </header>
   )
