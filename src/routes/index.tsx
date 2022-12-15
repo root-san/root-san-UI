92ed22b88d0ea948ed2f9b491f2b180baa48eb74
toshi-pono <66683209+toshi-pono@users.noreply.github.com>
2022-12-16 03:42:01 +0900

    ✨ helpページを仮置き

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,15 @@
 import { Routes, Route } from 'react-router-dom'
 
 import Top from '/@/pages/Top'
-import Register from '../pages/register'
+import Register from '/@/pages/Register'
+import Help from '/@/pages/Help'
 
 const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Top />} />
       <Route path="/register" element={<Register />} />
+      <Route path="/help" element={<Help />} />
     </Routes>
   )
 }
