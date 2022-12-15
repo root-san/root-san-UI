4e3296284cc6ae3abe7bbe7d518b880080347bdc
Rozelin <rozelin.dc@gmail.com>
2022-12-15 16:40:21 +0900

    storybookでtailwindが効くように

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import reactLogo from '/@/assets/react.svg'
+import '/@/styles/tailwind.scss'
 import '/@/styles/global.scss'
 import Button from '/@/components/Button'
 
