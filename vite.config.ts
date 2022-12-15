aa244539bdfb90355e8498573bbe1731ae0eb78e
toshi-pono <66683209+toshi-pono@users.noreply.github.com>
2022-12-15 23:06:57 +0900

    ✨ 雑にwpaを追加

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 import react from '@vitejs/plugin-react-swc'
 import path from 'path'
 
@@ -6,7 +7,47 @@ const srcPath = path.resolve(__dirname, 'src').replace(/\\/g, '/')
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      injectRegister: 'auto',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png'],
+      manifest: {
+        name: 'root3',
+        short_name: 'root3',
+        description: '\u5272\u308a\u52d8\u8a08\u7b97\u30a2\u30d7\u30ea',
+        theme_color: '#F1F2F6',
+        background_color: '#F1F2F6',
+        scope: '/',
+        start_url: '/',
+        display: 'standalone',
+        icons: [
+          {
+            src: 'icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'icon-256x256.png',
+            sizes: '256x256',
+            type: 'image/png',
+          },
+          {
+            src: 'icon-384x384.png',
+            sizes: '384x384',
+            type: 'image/png',
+          },
+          {
+            src: 'icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       '/@': srcPath,
