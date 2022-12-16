1505d2f014320bd79e1b8e2833749a2611127028
toshi-pono <66683209+toshi-pono@users.noreply.github.com>
2022-12-16 23:39:17 +0900

    ✨ 削除モーダルを表示

--- src/pages/Top/GroupCard.tsx
+++ src/pages/Top/GroupCard.tsx
@@ -1,8 +1,9 @@
 import { useRoom } from '/@/hooks/useRoom'
 import { useRoomStore } from '/@/hooks/useRoomStore'
 import DeletableCard from '/@/components/DeletableCard'
-
+import Modal from '/@/components/Modal'
 import apis from '/@/libs/apis'
+import { useState } from 'react'
 
 interface Props {
   roomId: string
@@ -21,6 +22,12 @@ const GroupCard = ({ roomId, myId, isPaid }: Props) => {
       console.error(e)
     }
   }
+  const [open, setOpen] = useState(false)
+  const onConfirmDelete = () => {
+    setOpen(true)
+  }
+
+  const [openDelete, setOpenDelete] = useState(false)
 
   const members = (room?.members ?? []).map((member) => member.name).join('・')
 
@@ -36,19 +43,57 @@ const GroupCard = ({ roomId, myId, isPaid }: Props) => {
   }
 
   return (
-    <DeletableCard onDelete={onDelete}>
-      <div className='w-full py-4 px-5'>
-        <p className='font-bold text-base truncate'>{room.name}</p>
-        <div className='flex text-gray-500 gap-x-2 text-xs'>
-          <p>
-            {room.createdAt !== undefined
-              ? new Date(room.createdAt).toLocaleDateString()
-              : ''}
-          </p>
-          <p className='truncate'>{members}</p>
+    <>
+      <DeletableCard
+        onDelete={onConfirmDelete}
+        open={openDelete}
+        onOpen={() => setOpenDelete(true)}
+        onClose={() => setOpenDelete(false)}
+      >
+        <div className='w-full py-4 px-5'>
+          <p className='font-bold text-base truncate'>{room.name}</p>
+          <div className='flex text-gray-500 gap-x-2 text-xs'>
+            <p>
+              {room.createdAt !== undefined
+                ? new Date(room.createdAt).toLocaleDateString()
+                : ''}
+            </p>
+            <p className='truncate'>{members}</p>
+          </div>
         </div>
-      </div>
-    </DeletableCard>
+      </DeletableCard>
+      <Modal
+        open={open}
+        onClose={() => {
+          setOpen(false)
+          setOpenDelete(false)
+        }}
+      >
+        <p className='w-full text-center font-bold text-lg'>
+          このグループを削除しますか？
+        </p>
+        <p className='w-full text-center text-sm mt-4'>
+          削除すると元には戻せません
+        </p>
+        <div className="flex gap-[17px] mt-9">
+          <button
+            onClick={onDelete}
+            className="w-full bg-red-50 h-12 text-red-500 font-bold rounded-lg hover:bg-red-100 active:bg-red-100"
+          >
+            削除
+          </button>
+          <button
+            onClick={() => {
+              setOpen(false)
+              setOpenDelete(false)
+            }}
+            className="w-full h-12 font-bold rounded-lg"
+          >
+            キャンセル
+          </button>
+        </div>
+      </Modal>
+    </>
   )
 }
 
