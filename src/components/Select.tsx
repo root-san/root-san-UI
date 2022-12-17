b1f6b9e5874b5099b21dc756c4ba94a19b273a95
Rozelin <rozelin.dc@gmail.com>
2022-12-17 13:01:51 +0900

    Create Select.tsx
    アイコンの位置おかしいかも

---
+++ src/components/Select.tsx
@@ -0,0 +1,35 @@
+import React, { useId } from 'react'
+
+import { Member } from '/@/libs/apis'
+
+interface Props {
+  title?: string
+  value: string
+  options: Member[]
+  onChange: (e: React.ChangeEvent<HTMLSelectElement>) => void
+}
+
+const Select = ({ title, value, options, onChange }: Props) => {
+  const id = useId()
+  return (
+    <div>
+      {title && (
+        <label htmlFor={id} className="block mb-3">
+          <span className="text-base font-bold">{title}</span>
+        </label>
+      )}
+      <select
+        id={id}
+        className="rounded-lg block w-full text-base py-3 px-4 outline-none"
+        value={value}
+        onChange={onChange}
+      >
+        {options.map((item) => (
+          <option value={item.id}>{item.name}</option>
+        ))}
+      </select>
+    </div>
+  )
+}
+
+export default Select
