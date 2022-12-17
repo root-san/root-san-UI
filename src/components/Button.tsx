73479194643542c353a44bc84ae53ad3be6afda0
Rozelin <rozelin.dc@gmail.com>
2022-12-17 17:48:39 +0900

    小さめの数字用のinput

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -4,19 +4,20 @@ interface Props {
   onClick: () => void
   white?: boolean
   warn?: boolean
+  className?: string
 }
 
-const Button = ({ text, onClick, disabled, white, warn }: Props) => {
+const Button = ({ text, onClick, disabled, white, warn, className }: Props) => {
   return (
     <button
       className={`
-        rounded-lg block w-full text-base py-3 px-4 outline-none bg-primary text-secondary font-bold 
+        rounded-lg block w-full text-base py-3 px-4 outline-none bg-primary text-secondary font-bold
         disabled:bg-gray-400 disabled:text-gray-200 disabled:cursor-not-allowed
         hover:bg-gray-900 transition duration-200 ease-in-out
         active:bg-gray-900 ${white ? 'bg-white text-primary' : ''} ${
         warn ? 'bg-red-50 text-red-500' : ''
-      }
-        `}
+      } ${className ?? ''}
+      `}
       onClick={onClick}
       disabled={disabled}
     >
