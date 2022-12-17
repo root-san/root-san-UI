3f59f6410828d6216d4ee0947abdcf53fccb8f86
Rozelin <rozelin.dc@gmail.com>
2022-12-17 21:20:25 +0900

    保存処理

--- src/pages/RegisterOnce/index.tsx
+++ src/pages/RegisterOnce/index.tsx
@@ -1,21 +1,45 @@
 import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { MdArrowBackIosNew } from 'react-icons/md'
+import { v4 as uuidv4 } from 'uuid'
+
+import { useOncePayStore } from '/@/hooks/useOncePayStore'
 
 import PageContainer from '/@/components/PageContainer'
 import Header from '/@/components/Header'
 import Input from '/@/components/Input'
 import Button from '/@/components/Button'
 import CountInput from './CountInput'
+import { OncePayTxn } from '/@/types/OncePay'
 
 const RegisterOnce = () => {
   const navigate = useNavigate()
   const [name, setName] = useState('')
   const [amount, setAmount] = useState('')
   const [count, setCount] = useState(2)
+  const { addOncePay } = useOncePayStore()
 
-  const submit = () => {
-    console.log('submit')
+  const submit = async () => {
+    const id = uuidv4()
+    const txns: OncePayTxn[] = []
+    const remainder = Number(amount) % count
+    const basePay = Math.floor(Number(amount) / count)
+    for (let i = 0; i < count; i++) {
+      txns.push({
+        id: uuidv4(),
+        name: 'hoge',
+        amount: basePay + (i < remainder ? 1 : 0),
+        isPaid: false,
+      })
+    }
+    await addOncePay({
+      id,
+      name,
+      amount: Number(amount),
+      createdAt: new Date().toString(),
+      txns,
+    })
+    navigate(`/once/${id}`)
   }
 
   return (
