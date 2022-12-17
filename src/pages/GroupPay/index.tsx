4cd435ab07fa5e1a02ed652c06bc9201c224e33a
Rozelin <rozelin.dc@gmail.com>
2022-12-17 15:12:34 +0900

    「均等にする」実装

--- src/pages/GroupPay/index.tsx
+++ src/pages/GroupPay/index.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import { useParams, Link } from 'react-router-dom'
-import { MdArrowBackIosNew } from 'react-icons/md'
+import { MdArrowBackIosNew, MdRestartAlt } from 'react-icons/md'
 
 import { useRoom } from '/@/hooks/useRoom'
 
@@ -9,6 +9,7 @@ import Header from '/@/components/Header'
 import Input from '/@/components/Input'
 import Button from '/@/components/Button'
 import Select from '/@/components/Select'
+import Tag from '/@/components/Tag'
 
 const GroupPay = () => {
   const { roomId } = useParams()
@@ -16,6 +17,8 @@ const GroupPay = () => {
   const [amount, setAmount] = useState('')
   const [eventAt, setEventAt] = useState('')
   const [receiver, setReceiver] = useState('')
+  /** id をキー, 支払額を値に持つ */
+  const [memberAmount, setMemberAmount] = useState<Record<string, string>>({})
 
   const submit = () => {
     console.log('hoge')
@@ -30,12 +33,28 @@ const GroupPay = () => {
     return <div>Loading...</div>
   }
 
+  const evenUp = () => {
+    const remainder = Number(amount) % room.members.length
+    const basePay = Math.floor(Number(amount) / room.members.length)
+    setMemberAmount(
+      Object.fromEntries(
+        room.members.map((member, idx) => {
+          if (idx < remainder) {
+            return [member.id, `${basePay + 1}`]
+          } else {
+            return [member.id, `${basePay}`]
+          }
+        })
+      )
+    )
+  }
+
   return (
     <PageContainer>
       <Header
         title="出費"
         left={
-          <Link to={`/group/${roomId}`} className='text-[24px]'>
+          <Link to={`/group/${roomId}`} className="text-[24px]">
             <MdArrowBackIosNew />
           </Link>
         }
@@ -74,13 +93,30 @@ const GroupPay = () => {
           />
         </div>
         <div className="flex flex-col items-start w-full gap-4">
-          <div className="text-base font-bold">分配</div>
+          <div className="flex flex-row w-full gap-4">
+            <div className="flex flex-row flex-1 gap-2">
+              <div className="text-base font-bold">分配</div>
+              <Tag isOptional />
+            </div>
+            <div className="flex flex-row gap-1 text-selected" onClick={evenUp}>
+              <MdRestartAlt className="text-[24px]" />
+              <div className="text-base">均等にする</div>
+            </div>
+          </div>
           <div className="flex flex-col w-full gap-2">
             {room.members.map((member) => (
               <div className="flex flex-row items-center gap-4">
                 <div className="text-base flex-1">{member.name}</div>
                 <div className="w-[120px]">
-                  <Input type="number" value="100" onChange={(e) => {}} />
+                  <Input
+                    type="number"
+                    value={memberAmount[member.id]}
+                    onChange={(e) => {
+                      const newVal = memberAmount
+                      newVal[member.id] = e.target.value
+                      setMemberAmount(newVal)
+                    }}
+                  />
                 </div>
               </div>
             ))}
