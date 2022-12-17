1386cc58c09c85e548453f17b877db06c6cbb318
toshi-pono <66683209+toshi-pono@users.noreply.github.com>
2022-12-18 03:26:22 +0900

    🚧 ページ遷移できないんだよなー

--- src/components/AnimateBody.tsx
+++ src/components/AnimateBody.tsx
@@ -1,18 +1,38 @@
 import { motion } from 'framer-motion'
+import { useHistory } from '/@/providers/HistoryProvider'
 import { useAnimateSetting } from '/@/hooks/useAnimateSetting'
-
 interface Props {
   children: React.ReactNode
-  back?: boolean // 戻る方向のアニメーション
 }
 
-const AnimateBody = ({ children, back }: Props) => {
-  const { pageDuration } = useAnimateSetting()
+const animateDirection = (before: number[], current: number[]) => {
+  if (before[1] > current[1]) {
+    // Up
+    return { y: '-100%' }
+  } else if (before[1] < current[1]) {
+    // Down
+    return { y: '100%' }
+  }
+
+  if (before[0] > current[0]) {
+    // Left
+    return { x: '100%' }
+  }
+  // Right
+  return { x: '-100%' }
+}
+
+const AnimateBody = ({ children }: Props) => {
+  const { pageDuration, pathToPageLevel } = useAnimateSetting()
+  const { history } = useHistory()
+  const before = pathToPageLevel(history[1])
+  const current = pathToPageLevel(history[0])
+
   return (
     <motion.div
-      initial={back ? { x: '-100%' } : { x: '100%' }}
-      animate={{ x: '0%' }}
-      exit={back ? { x: '100%' } : { x: '-100%' }}
+      initial={animateDirection(before, current)}
+      animate={{ x: '0%', y: '0%' }}
+      exit={animateDirection(before, current)}
       transition={{ duration: pageDuration }}
     >
       {children}
--- src/providers/app.tsx
+++ src/providers/app.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter } from 'react-router-dom'
 import { SWRConfig } from 'swr'
 
 import { localStorageProvider } from './localStorageProvider'
+import HistoryProvider from './HistoryProvider'
 
 interface AppProviderProps {
   children: React.ReactNode
@@ -12,7 +13,9 @@ const AppProviders = ({ children }: AppProviderProps) => {
   return (
     <React.Suspense fallback={<div>Loading...</div>}>
       <SWRConfig value={{ provider: localStorageProvider }}>
-        <BrowserRouter>{children}</BrowserRouter>
+        <BrowserRouter>
+          <HistoryProvider>{children}</HistoryProvider>
+        </BrowserRouter>
       </SWRConfig>
     </React.Suspense>
   )
