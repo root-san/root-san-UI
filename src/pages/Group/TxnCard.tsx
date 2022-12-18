934fe96b6bff8aad9e27fc134ca52fb6acfbac5c
toshi-pono <66683209+toshi-pono@users.noreply.github.com>
2022-12-18 10:42:49 +0900

    🎉 編集ページの作成

--- src/pages/Group/TxnCard.tsx
+++ src/pages/Group/TxnCard.tsx
@@ -8,10 +8,11 @@ import { toDateTime } from '/@/libs/date'
 interface Props {
   event: Event
   payer: string
+  roomId: string
   onDelete: (id: string) => void
 }
 
-const TxnCard = ({ event, payer, onDelete }: Props) => {
+const TxnCard = ({ event, payer, onDelete, roomId }: Props) => {
   const [isModalOpen, setIsModalOpen] = useState(false)
   const [isDeleteOpen, setIsDeleteOpen] = useState(false)
 
@@ -32,6 +33,7 @@ const TxnCard = ({ event, payer, onDelete }: Props) => {
         onOpen={() => setIsDeleteOpen(true)}
         onClose={() => setIsDeleteOpen(false)}
         onDelete={onConfirmDelete}
+        to={`/group/${roomId}/edit/${event.id}`}
       >
         <div className='w-full py-4 px-5 relative'>
           <div
--- src/pages/Group/TxnList.tsx
+++ src/pages/Group/TxnList.tsx
@@ -22,8 +22,8 @@ const TxnList = ({ room }: Props) => {
 
   return (
     <div className='flex flex-col gap-3'>
-      {
-        room.events.sort((a, b) => {
+      {room.events
+        .sort((a, b) => {
           // eventAtの日付のみ(時間は無視)を第一条件、CreatedAtの時間を第二条件にして降順ソート
           const ad = new Date(a.eventAt)
           const bd = new Date(b.eventAt)
@@ -42,21 +42,23 @@ const TxnList = ({ room }: Props) => {
               return 0
             }
           }
-        }).map((event) => (
-        <TxnCard
-          key={event.id}
-          event={event}
-          payer={
-            event.eventType === 'outer'
-              ? `${toName(room.members, event.txns[0].payer)}が支払い`
-              : `${toName(room.members, event.txns[0].payer)}が${toName(
-                  room.members,
-                  event.txns[0].receiver
-                )}に支払い`
-          }
-          onDelete={onDelete}
-        />
-      ))}
+        })
+        .map((event) => (
+          <TxnCard
+            key={event.id}
+            event={event}
+            roomId={room.id}
+            payer={
+              event.eventType === 'outer'
+                ? `${toName(room.members, event.txns[0].payer)}が支払い`
+                : `${toName(room.members, event.txns[0].payer)}が${toName(
+                    room.members,
+                    event.txns[0].receiver
+                  )}に支払い`
+            }
+            onDelete={onDelete}
+          />
+        ))}
     </div>
   )
 }
