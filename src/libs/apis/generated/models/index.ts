eca117b131f8d07baadb44d616ab9f11b4fd980c
toshi-pono <66683209+toshi-pono@users.noreply.github.com>
2023-06-27 15:09:32 +0900

    ⚡ generate API

---
+++ src/libs/apis/generated/models/index.ts
@@ -0,0 +1,11 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './Event';
+export * from './EventRequestBody';
+export * from './EventType';
+export * from './Member';
+export * from './Result';
+export * from './Room';
+export * from './RoomDetails';
+export * from './RoomRuquestBody';
+export * from './Txn';
